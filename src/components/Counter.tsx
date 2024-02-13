28962a4c09a9ae255937e93aa4cf4b9c021ed667
Mohammed Hassan <mohammedBhassan@proton.me>
2024-02-13 21:00:19 +0300

    Added the counter component

---
+++ src/components/Counter.tsx
@@ -0,0 +1,38 @@
+"use client";
+import {useState} from "react"
+
+
+
+export default function Counter() {
+const [count,setCount] = useState<number>(0)
+
+    const handleIncrement = () : void => {
+        setCount(count + 1 )
+    }
+    const handleDecrement = () : void => {
+        if(count === 0) {
+            setCount(0);
+        }else {
+            setCount(count - 1)
+        }
+  
+    }
+
+    return (<div className="border border-solid  border-slate-500 rounded-sm p-2 h-56 w-56 flex flex-col  gap-4 place-content-center">
+    
+
+
+            <div className="counter-view py-2">
+                <h1 className="text-center">Current Count:  {count}</h1>
+            </div>
+            <div className="btns-container flex  gap-4 place-content-center">
+
+                <button className="btns-container__btn border border-solid  border-slate-500 rounded-sm bg-blue-700 px-5" type="button" onClick={handleIncrement} > + </button>
+                <button className="btns-container__btn border border-solid  border-slate-500 rounded-sm bg-red-700 px-5" type="button" onClick={handleDecrement} > - </button>
+            </div>
+
+        </div>
+    )
+
+
+}
